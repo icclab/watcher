a3e4b5455ee585c2b6da1188bf7a3f02d4059218
cima <cima@zhaw.ch>
2016-02-23 11:53:27 +0100

    [src] smart consolidation algorithm implementation

--- watcher/decision_engine/strategy/strategies/smart_consolidation.py
+++ watcher/decision_engine/strategy/strategies/smart_consolidation.py
@@ -30,42 +30,109 @@ class SmartStrategy(base.BaseStrategy):
                  osc=None):
         super(SmartStrategy, self).__init__(name, description, osc)
 
-    def add_migration(self, migration_type, vm_uuid,
-                      src_hypervisor, dst_hypervisor,
-                      model):
-        # TODO unmap & map VM to model environment after migration
+    def add_migration(self, vm, src_hypervisor,
+                      dst_hypervisor, model):
+        model.unmap(src_hypervisor, vm)
+        model.map(dst_hypervisor, vm)
+        migration_type = 'live'
         params = {'migration_type': migration_type,
                   'src_hypervisor': src_hypervisor,
                   'dst_hypervisor': dst_hypervisor}
         self.solution.add_action(action_type=self.MIGRATION,
-                                 resource_id=vm_uuid,
+                                 resource_id=vm.uuid,
                                  input_parameters=params)
 
     def get_prediction_model(self, model):
         return deepcopy(model)
 
-    def get_vm_utilization(self, vm_uuid, model):
+    def get_vm_utilization(self, vm, model):
         # TODO (gaea)
-        # return (cpu_util, ram_util)
+        # return {'cpu': cpu, 'ram': ram, 'disk': disk}
         pass
 
-    def get_hypervisor_utilization(self, hypervisor_uuid, model):
+    def get_hypervisor_utilization(self, hypervisor, model):
         # TODO (gaea)
-        # return (cpu_util, ram_util)
+        # return {'cpu': cpu, 'ram': ram, 'disk': disk}
         pass
 
-    def get_hypervisor_capacity(self, hypervisor_uuid, model):
+    def get_hypervisor_capacity(self, hypervisor, model):
         # TODO (gaea)
-        # return (cpu_capacity, ram_capacity)
+        # return {'cpu': cpu, 'ram': ram, 'disk': disk}
         pass
+
+    def is_overloaded(self, hypervisor, model):
+        hypervisor_capacity = self.get_hypervisor_capacity(hypervisor, model)
+        hypervisor_utilization = self.get_hypervisor_utilization(
+            hypervisor, model)
+        metrics = ['cpu']
+        for m in metrics:
+            if hypervisor_utilization[m] > hypervisor_capacity[m]:
+                return True
+        return False
+
+    def vm_fits(self, vm, hypervisor, model):
+        hypervisor_capacity = self.get_hypervisor_capacity(hypervisor, model)
+        hypervisor_utilization = self.get_hypervisor_utilization(
+            hypervisor, model)
+        vm_utilization = self.get_vm_utilization(vm, model)
+        metrics = ['cpu', 'ram', 'disk']
+        for m in metrics:
+            if vm_utilization[m] + \
+                    hypervisor_utilization[m] > hypervisor_capacity[m]:
+                return False
+        return True
 
     def offload_phase(self, model):
-        # TODO (cima)
-        pass
+        '''
+        Offload phase performing first-fit based bin packing to offload
+        overloaded hypervisors. This is done in a fashion of moving
+        the least CPU utilized VM first as live migration these
+        generaly causes less troubles.
+        '''
+
+        sorted_hypervisors = sorted(
+            model.get_all_hypervisors(),
+            key=lambda x: self.get_hypervisor_utilization(x, model)['cpu'])
+        for hypervisor in reversed(sorted_hypervisors):
+            if self.is_overloaded(hypervisor, model):
+                for vm in sorted(model.get_mapping().get_node_vms_from_id(
+                        hypervisor.uuid),
+                        key=lambda x: self.get_vm_utilization(
+                        vm, model)['cpu']):
+                    for dst_hypervisor in sorted_hypervisors:
+                        if self.vm_fits(vm, dst_hypervisor, model):
+                            self.add_migration(vm, hypervisor, dst_hypervisor)
+                            break
+                    if not self.is_overloaded(hypervisor, model):
+                        break
 
     def consolidation_phase(self, model):
-        # TODO (cima)
-        pass
+        '''
+        Consolidation phase performing first-fit based bin packing.
+        First, hypervisors with the lowest cpu utilization are consolidated
+        by moving their load to hypervisors with the highest cpu utilization
+        which can accomodate the load. In this phase the most cpu utilizied
+        VMs are prioritizied as their load is more difficult to accomodate
+        in the system than less cpu utilizied VMs which can be later used
+        to fill smaller CPU capacity gaps.
+        '''
+
+        sorted_hypervisors = sorted(
+            model.get_all_hypervisors(),
+            key=lambda x: self.get_hypervisor_utilization(x, model)['cpu'])
+        asc = 0
+        for hypervisor in sorted_hypervisors:
+            for vm in sorted(model.get_mapping().get_node_vms_from_id(
+                    hypervisor.uuid),
+                    key=lambda x: self.get_vm_utilization(vm, model)['cpu'],
+                    reverse=True):
+                dsc = len(sorted_hypervisors) - 1
+                for dst_hypervisor in reversed(sorted_hypervisors):
+                    if self.vm_fits(vm, dst_hypervisor, model):
+                        self.add_migration(vm, hypervisor, dst_hypervisor)
+                    dsc = dsc - 1
+                    if asc >= dsc:
+                        break
 
     def execute(self, original_model):
         LOG.info("Executing Smart Strategy")
